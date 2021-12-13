eba9f5fe42249c4c4530778e9af12c3e438f1073
Michael Clark <mclarkgb@gmail.com>
2021-12-13 20:10:23 +0000

    a little update to the working example

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,78 @@
     <li>Start adding VueUse and removing some of the code in the utils</li>
   </ul>
 
-  <grid-layout v-model:layout="layout" :row-height="20">
-    <grid-item
-      v-for="item in layout"
-      :key="item.i"
-      :x="item.x"
-      :y="item.y"
-      :w="item.w"
-      :h="item.h"
-      :i="item.i"
-    >
-      <div
-        style="
-          width: 100%;
-          height: 100%;
-          text-align: center;
-          border: 1px solid black;
-        "
-      >
+  <div>
+    <h1>Settings</h1>
+    <h5>Layout</h5>
+    <div>
+      <label for="autoSize">AutoSize</label>
+      <input id="autoSize" v-model="layoutSettings.autoSize" type="checkbox" />
+    </div>
+    <div>
+      <label for="colNum">colNum</label>
+      <input id="colNum" v-model="layoutSettings.colNum" type="number" />
+    </div>
+    <div>
+      <label for="rowHeight">rowHeight</label>
+      <input id="rowHeight" v-model="layoutSettings.rowHeight" type="number" />
+    </div>
+    <div>
+      <label for="maxRows">maxRows</label>
+      <input id="maxRows" v-model="layoutSettings.maxRows" type="number" />
+    </div>
+    <div>
+      <label for="isDraggable">isDraggable</label>
+      <input id="isDraggable" v-model="layoutSettings.isDraggable" type="checkbox" />
+    </div>
+    <div>
+      <label for="isResizable">isResizable</label>
+      <input id="isResizable" v-model="layoutSettings.isResizable" type="checkbox" />
+    </div>
+    <div>
+      <label for="isResizable">isMirrored</label>
+      <input id="isResizable" v-model="layoutSettings.isMirrored" type="checkbox" />
+    </div>
+    <div>
+      <label for="useCssTransforms">useCssTransforms</label>
+      <input id="useCssTransforms" v-model="layoutSettings.useCssTransforms" type="checkbox" />
+    </div>
+    <div>
+      <label for="verticalCompact">verticalCompact</label>
+      <input id="verticalCompact" v-model="layoutSettings.verticalCompact" type="checkbox" />
+    </div>
+    <div>
+      <label for="responsive">responsive</label>
+      <input id="responsive" v-model="layoutSettings.responsive" type="checkbox" />
+    </div>
+    <div>
+      <label for="preventCollision">preventCollision</label>
+      <input id="preventCollision" v-model="layoutSettings.preventCollision" type="checkbox" />
+    </div>
+    <div>
+      <label for="useStyleCursor">useStyleCursor</label>
+      <input id="useStyleCursor" v-model="layoutSettings.useStyleCursor" type="checkbox" />
+    </div>
+  </div>
+
+  <grid-layout
+    v-model:layout="layout"
+    :auto-size="layoutSettings.autoSize"
+    :col-num="layoutSettings.colNum"
+    :row-height="layoutSettings.rowHeight"
+    :max-rows="layoutSettings.maxRows"
+    :is-draggable="layoutSettings.isDraggable"
+    :is-resizable="layoutSettings.isResizable"
+    :is-mirrored="layoutSettings.isMirrored"
+    :use-css-transforms="layoutSettings.useCssTransforms"
+    :vertical-compact="layoutSettings.verticalCompact"
+    :responsive="layoutSettings.responsive"
+    :prevent-collision="layoutSettings.preventCollision"
+    :use-style-cursor="layoutSettings.useStyleCursor"
+  >
+    <grid-item v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i">
+      <div style="width: 100%; height: 100%; text-align: center; border: 1px solid black">
         <span>{{ item.i }}</span>
+        <p>Maybe put layout item settings here and allow configuring them in the demo</p>
       </div>
     </grid-item>
   </grid-layout>
@@ -39,6 +92,21 @@
 import { ref } from 'vue'
 import GridItem from './components/GridItem.vue'
 import GridLayout from './components/GridLayout.vue'
+
+const layoutSettings = ref({
+  autoSize: true,
+  colNum: 12,
+  rowHeight: 50,
+  maxRows: Infinity,
+  isDraggable: true,
+  isResizable: true,
+  isMirrored: false,
+  useCssTransforms: true,
+  verticalCompact: true,
+  responsive: false,
+  preventCollision: false,
+  useStyleCursor: true,
+})
 
 const layout = ref([
   { x: 0, y: 0, w: 2, h: 2, i: '0' },
