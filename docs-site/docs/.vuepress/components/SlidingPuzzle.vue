2b9d637552df19a871eea17e71f922c19b74cd25
Michael Clark <mclarkgb@gmail.com>
2021-12-26 16:02:06 +0000

    weird vscode setup with this laptop

--- docs-site/docs/.vuepress/components/SlidingPuzzle.vue
+++ docs-site/docs/.vuepress/components/SlidingPuzzle.vue
@@ -1,40 +1,51 @@
 <template>
   <h5>Still todo:</h5>
   <ul>
-    <li>MaxRows only applies to items when dragging or resizing, if you drag to position 0,0 when the column already has the max rows, it will allow it and the col will have one over the max rows</li>
-    <li>Figure out a way to do rules on whether or not something can move. Here we want to validate whether something can move to a place</li>
-    <li>https://github.com/MikesGlitch/agnostos/blob/master/pet-dating/sapper-ui/src/components/slidingPuzzle.svelte</li>
-    <li>May need to create an event to allow re-structuring on the layout based on a drag position. Instead of it default to a "stack" it needs to allow configuration to potentially allow the tile to switch places</li>
+    <li>
+      MaxRows only applies to items when dragging or resizing, if you drag to position 0,0 when the column already has
+      the max rows, it will allow it and the col will have one over the max rows
+    </li>
+    <li>
+      Figure out a way to do rules on whether or not something can move. Here we want to validate whether something can
+      move to a place
+    </li>
+    <li>
+      https://github.com/MikesGlitch/agnostos/blob/master/pet-dating/sapper-ui/src/components/slidingPuzzle.svelte
+    </li>
+    <li>
+      May need to create an event to allow re-structuring on the layout based on a drag position. Instead of it default
+      to a "stack" it needs to allow configuration to potentially allow the tile to switch places
+    </li>
   </ul>
 
-<div ref="tileGameContainer">
-  <grid-layout
-    class="tile-game"
-    :style="tileGameStyle"
-    v-model:layout="layout"
-    :margin="[0, 0]"
-    :verticalCompact="true"
-    :colNum="numTiles"
-    :maxRows="numTiles"
-    :is-draggable="true"
-    :is-resizable="false"
-    :rowHeight="tileWidth"
-  >
-    <grid-item
-      :class="`tile tile--${item.tile}`"
-      v-for="item in layout"
-      :key="item.i"
-      :x="item.x"
-      :y="item.y"
-      :w="item.w"
-      :h="item.h"
-      :i="item.i"
+  <div ref="tileGameContainer">
+    <grid-layout
+      class="tile-game"
+      :style="tileGameStyle"
+      v-model:layout="layout"
+      :margin="[0, 0]"
+      :verticalCompact="true"
+      :colNum="numTiles"
+      :maxRows="numTiles"
+      :is-draggable="true"
+      :is-resizable="false"
+      :rowHeight="tileWidth"
     >
-      <div style="width: 100%; height: 100%; text-align: center; border: 1px solid black">
-        <span>{{ item.i }}</span>
-      </div>
-    </grid-item>
-  </grid-layout>
+      <grid-item
+        v-for="item in layout"
+        :key="item.i"
+        :class="`tile tile--${item.tile}`"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+      >
+        <div style="width: 100%; height: 100%; text-align: center; border: 1px solid black">
+          <span>{{ item.i }}</span>
+        </div>
+      </grid-item>
+    </grid-layout>
   </div>
   <pre>
     {{ layout }}
@@ -48,7 +59,7 @@ import { GridLayout, GridItem } from 'vue-griddy'
 const tileWidth = ref(100)
 const numTiles = 3
 const tileGameStyle = computed(() => {
-  const fullWidth = tileWidth * numTiles
+  const fullWidth = tileWidth.value * numTiles
   return { width: fullWidth + 'px', height: fullWidth + 'px' }
 })
 
@@ -66,11 +77,11 @@ const layout = ref([
   { x: 2, y: 2, w: 1, h: 1, i: '8', tile: 'empty' }, // Empty - needs to be empty tile to allow for dragging on top of
 ])
 
-const setupBoard = () => {  
-    if (tileGameContainer.value) {
-      // Adjusting the size of the tiles etc based on the size of the container
-      tileWidth.value = tileGameContainer.value.offsetWidth / numTiles
-    }
+const setupBoard = () => {
+  if (tileGameContainer.value) {
+    // Adjusting the size of the tiles etc based on the size of the container
+    tileWidth.value = tileGameContainer.value.offsetWidth / numTiles
+  }
 }
 
 onMounted(() => {
