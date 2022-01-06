4e6d24fb3a3b5e86c439897adba1d2e1fa8a6691
Michael Clark <mclarkgb@gmail.com>
2022-01-06 18:33:07 +0000

    nuxt 3 example, working in dev but not in prod bundle - due to vue duplication?  need to dedupe or resolve

--- examples/nuxt3-app/app.vue
+++ examples/nuxt3-app/app.vue
@@ -1,33 +1,6 @@
 <template>
-  <h1>DON'T FORGET TO TAKE FROM THE VUE3 BRANCH OF VUE-GRID-LAYOUT</h1>
-  <h5>TODOS:</h5>
-  <ul>
-    <li>Vuepress website with code integration into the library - will be used for examples</li>
-    <li>Copy from gridstack API and make this code better</li>
-    <li>Remove any features you dont want and make some examples</li>
-    <li>Rename the component and do a full separation from vue-grid-layout</li>
-    <li>Potentially start adding VueUse and removing some of the code in the utils</li>
-    <li>Move this page to website documentation? It's basically what I want but I need the package separate</li>
-    <li>Responsive mode doesn't work with over 12 cols. It's very annoying. Fix it.</li>
-    <li>Good idea for an example - slider puzzle.</li>
-    <li>Good idea for an example - Dashboard widgets.</li>
-    <li>Bit of inspiration https://github.com/react-grid-layout/react-grid-layout</li>
-    <li>
-      I think whatever we do we'll need to use an event bus. There's now ay to use emit since we don't have access to
-      slot contents - not unless we change the way the component is structured. Probably just use vueuse
-      https://v3.vuejs.org/guide/migration/events-api.html#event-bus
-    </li>
-    <li>
-      Find a way to make vite inject the CSS into the bundle in lib mode -
-      <a href="https://github.com/vitejs/vite/issues/4345">https://github.com/vitejs/vite/issues/4345</a>
-    </li>
-    <li>
-      Fix the nuxt 3 app example - ice not working when doing a build, then preview with SSR bundle. Something to do
-      with vue duplication
-    </li>
-  </ul>
-
   <div>
+    <h1>DOESN'T CURRENTLY WORK WHEN RUNNING BUILD THEN PRIVEW - SOMETHING TO DO WITH ICE - IT'S TO DO WITH THE VUE VERSION - BUNDLE - https://github.com/vuejs/vue-next/issues/4344</h1>
     <h1>Settings</h1>
     <h5>Layout</h5>
     <div>
@@ -82,45 +55,40 @@
       <label for="useStyleCursor">useStyleCursor</label>
       <input id="useStyleCursor" v-model="layoutSettings.useStyleCursor" type="checkbox" />
     </div>
-    <button @click="reloadWithDelay">Reload with delay</button>
   </div>
-
   <div :style="{ width: containerWidth + '%' }">
-    <grid-layout
-      v-model:layout="layout"
-      :auto-size="layoutSettings.autoSize"
-      :col-num="layoutSettings.colNum"
-      :row-height="layoutSettings.rowHeight"
-      :max-rows="layoutSettings.maxRows"
-      :is-draggable="layoutSettings.isDraggable"
-      :is-resizable="layoutSettings.isResizable"
-      :is-mirrored="layoutSettings.isMirrored"
-      :use-css-transforms="layoutSettings.useCssTransforms"
-      :vertical-compact="layoutSettings.verticalCompact"
-      :responsive="layoutSettings.responsive"
-      :prevent-collision="layoutSettings.preventCollision"
-      :use-style-cursor="layoutSettings.useStyleCursor"
-    >
-      <grid-item v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i">
-        <div style="width: 100%; height: 100%; text-align: center; border: 1px solid black">
-          <span>{{ item.i }}</span>
-          <p>Maybe put layout item settings here and allow configuring them in the demo</p>
-        </div>
-      </grid-item>
-    </grid-layout>
+    <ClientOnly>
+      <grid-layout
+        v-model:layout="layout"
+        :auto-size="layoutSettings.autoSize"
+        :col-num="layoutSettings.colNum"
+        :row-height="layoutSettings.rowHeight"
+        :max-rows="layoutSettings.maxRows"
+        :is-draggable="layoutSettings.isDraggable"
+        :is-resizable="layoutSettings.isResizable"
+        :is-mirrored="layoutSettings.isMirrored"
+        :use-css-transforms="layoutSettings.useCssTransforms"
+        :vertical-compact="layoutSettings.verticalCompact"
+        :responsive="layoutSettings.responsive"
+        :prevent-collision="layoutSettings.preventCollision"
+        :use-style-cursor="layoutSettings.useStyleCursor"
+      >
+        <grid-item v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i">
+          <div style="width: 100%; height: 100%; text-align: center; border: 1px solid black">
+            <span>{{ item.i }}</span>
+          </div>
+        </grid-item>
+      </grid-layout>
+    </ClientOnly>
   </div>
-  <pre>
-    {{ layout }}
-  </pre>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import { ref } from 'vue'
-import GridItem from './components/GridItem.vue'
-import GridLayout from './components/GridLayout.vue'
+import { GridLayout, GridItem } from 'vue-tile-layout'
+import 'vue-tile-layout/style'
 
 const containerWidth = ref(100)
-
 const layoutSettings = ref({
   autoSize: true,
   colNum: 12,
@@ -135,14 +103,6 @@ const layoutSettings = ref({
   preventCollision: false,
   useStyleCursor: true,
 })
-
-const reloadWithDelay = () => {
-  const oldLayout = [...layout.value]
-  layout.value = []
-  setTimeout(() => {
-    layout.value = oldLayout
-  }, 3000)
-}
 
 const layout = ref([
   { x: 0, y: 0, w: 2, h: 2, i: '0' },
