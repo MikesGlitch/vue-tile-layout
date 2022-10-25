2364fcb19a657650ae3d5853f479a40273047a29
Michael Clark <mclarkgb@gmail.com>
2022-10-25 17:08:18 +0100

    adding expiremental webcomponent

---
+++ experimental/vue-tile-layout/src/my-element.ts
@@ -0,0 +1,47 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('my-element')
+export class MyElement extends LitElement {
+  /**
+   * Copy for the read the docs hint.
+   */
+  @property()
+  test = 'sup bitch'
+
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: Number })
+  count = 0
+
+  render() {
+    return html`
+      <slot></slot>
+      <div class="card">
+        <button @click=${this._onClick} part="button">
+          count is ${this.count}
+        </button>
+      </div>
+      <p>${this.test}</p>
+    `
+  }
+
+  private _onClick() {
+    this.count++
+  }
+
+  static styles = css``
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-element': MyElement
+  }
+}
